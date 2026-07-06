d570747422660c19f510f96ecacfc03454272c66
kevmack7323-tech <kevmack7323@gmail.com>
2026-07-06 10:55:43 -0400

    Initial commit: basic server setup

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const PORT = 3000;
+const app = express()
+
+
+
+
+
+app.listen( PORT, ()=>{
+    console.log('listening on port..', PORT)
+});
+
+git init
+git add .
+git commit -m "Initial commit: basic server setup"
+git branch -M main
+git remote add origin https://github.com/kevmack7323-tech/week12Excercise
+git push -u origin main
