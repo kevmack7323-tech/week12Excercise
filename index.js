a42ea0f8c2061b27aa5047a5fd4fe4ad8628fb35
kevmack7323-tech <kevmack7323@gmail.com>
2026-07-06 13:04:41 -0400

    Midlleware implemented

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const PORT = 3000;
-const app = express()
+const app = express();
 
-
+app.use('/', async(req, res, next) =>{
+    next();
+});
 
 
 
 app.listen( PORT, ()=>{
     console.log('listening on port..', PORT)
 });
-
