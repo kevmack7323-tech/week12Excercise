57e6b19c999617b1a6f4587c53e4e793042398ec
kevmack7323-tech <kevmack7323@gmail.com>
2026-07-06 10:56:19 -0400

    push

--- index.js
+++ index.js
@@ -10,9 +10,3 @@ app.listen( PORT, ()=>{
     console.log('listening on port..', PORT)
 });
 
-git init
-git add .
-git commit -m "Initial commit: basic server setup"
-git branch -M main
-git remote add origin https://github.com/kevmack7323-tech/week12Excercise
-git push -u origin main
