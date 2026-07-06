cb1460f7d2f7728a3182222ecfe5c78cebf2285d
IsaiasRT <isaiasruizt@gmail.com>
2026-07-06 15:05:16 -0400

    Schema updated and update controller added

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,19 +1,43 @@
-export const getAllRoutes = (req, res) ={
+import Route from "../models/routes.js";
+
+export const getAllRoutes = (req, res) = {
 
 }
 
-export const getRoutebyID = (req, res) ={
-    
+export const getRoutebyID = (req, res) = {
+
 }
 
-export const createRoute = (req, res) ={
-    
+export const createRoute = (req, res) = {
+
 }
 
-export const updateRoute = (req, res) ={
-    
+
+export const updateRoute = async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const updateData = req.body;
+
+        const updatedRoute = await Route.findByIdAndUpdate(
+            id,
+            updateData,
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedRoute) {
+            return res.status(404).json({ message: "Route not found" });
+        }
+
+        res.status(200).json(updatedRoute);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
-export const deleteRoute = (req,res) ={
+
+
+
+export const deleteRoute = (req, res) = {
 
 }
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import dotenv from "dotenv";
 import express from 'express';
 import connectDB from "db.js";
+import productControllers from "routes.js"
 dotenv.config();
 const PORT = 3000;
 const app = express();
+
+
+
+app.use('/routes');
 
 app.use('/', async (req, res, next) => {
     next();
