1690a0117716bfaab7589a207631668b4ad8f83e
irfanwalele <irfanwalele@gmail.com>
2026-07-06 11:52:45 -0400

    db.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import dotenv from "dotenv";
 import express from 'express';
+import connectDB from "./db";
+dotenv.config();
 const PORT = 3000;
 const app = express()
 
@@ -10,9 +13,9 @@ app.listen( PORT, ()=>{
     console.log('listening on port..', PORT)
 });
 
-git init
-git add .
-git commit -m "Initial commit: basic server setup"
-git branch -M main
-git remote add origin https://github.com/kevmack7323-tech/week12Excercise
-git push -u origin main
+// git init
+// git add .
+// git commit -m "Initial commit: basic server setup"
+// git branch -M main
+// git remote add origin https://github.com/kevmack7323-tech/week12Excercise
+// git push -u origin main
